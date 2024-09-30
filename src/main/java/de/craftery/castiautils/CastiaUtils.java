3728b74e1a97df25c54707357931378e77f0fe24
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-09-30 16:04:36 +0200

    Ready, Set, Go!

---
+++ src/main/java/de/craftery/castiautils/CastiaUtils.java
@@ -0,0 +1,10 @@
+package de.craftery.castiautils;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CastiaUtils implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
