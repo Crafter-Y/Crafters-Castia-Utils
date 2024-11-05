9056de4521b51a939ff47c79ca5d76bca8a5eb2e
Crafter-Y <mrcrafter.yt1@gmail.com>
2024-11-05 23:10:30 +0100

    with gh action

--- src/main/java/de/craftery/castiautils/chestshop/ContainerValueProvider.java
+++ src/main/java/de/craftery/castiautils/chestshop/ContainerValueProvider.java
@@ -42,6 +42,7 @@ public class ContainerValueProvider {
                 !(screen instanceof GenericContainerScreen) &&
                 !(screen instanceof ShulkerBoxScreen)
         ) return;
+        if (screen.getTitle().getString().length() == 2 && screen.getTitle().getString().charAt(0) == 57344 && screen.getTitle().getString().charAt(1) == 57856) return; // hide anyway in chestshops
 
         int textX = screen.x;
         int textY = screen.y - 10;
